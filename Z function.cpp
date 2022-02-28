6b18f6663bcd36030d22fb40332554f9a39bddc4
masud45827 <60808203+masud45827@users.noreply.github.com>
2022-02-28 05:46:26 -0800

    Create Z function.cpp

---
+++ Z function.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef     long long    ll;
+//typedef     unsigned long long    ull;
+typedef     vector<int> vi;
+typedef     vector<long long> vl;
+typedef     pair<int, int>pi;
+typedef     pair<long long, long long>pl;
+#define F   first
+#define S   second
+#define pb  push_back
+#define     all(x)      x.begin() , x.end()
+#define mod 1000000007
+#define       FOR(i,a,b) for(i=a;i<=b;i++)
+#define     mem(a)      memset(a , 0 ,sizeof a)
+#define     memn(a)     memset(a , -1 ,sizeof a)
+#ifdef LOCAL
+#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+void err(istream_iterator<string> it) {cout << endl ;}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+    cerr << *it << " = " << a << ' ' ;
+    err(++it, args...);
+}
+#else
+#define error(args...)
+#endif
+vector<int> z_function(string s) {
+    int n = (int) s.length();
+    vector<int> z(n);
+    for (int i = 1, l = 0, r = 0; i < n; ++i) {
+        if (i <= r) z[i] = min (r - i + 1, z[i - l]);
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]]) ++z[i];
+        if (i + z[i] - 1 > r){
+            l = i, r = i + z[i] - 1;
+          }
+    }
+    return z;
+}
+int main() {
+  int test = 1, fac = 1;
+  scanf("%d", &test);
+  while (test--) {
+      string s;
+      cin>>s;
+       vector<int>v=z_function(s);
+       for(int x:v) cout<<x<<" ";
+        cout<<endl;
+  }
+  return 0;
+}
+
